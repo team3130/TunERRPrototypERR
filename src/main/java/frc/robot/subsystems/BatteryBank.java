7324a5f9ea766c35a1ba0b88d21cbf43fc96f2e0
CorianderSeed <github.unscrew076@slmails.com>
2026-01-29 14:59:59 -0600

    saved initial rough work on battery bank, created account and batterybank classes and constructors and trying to figure out logic.

---
+++ src/main/java/frc/robot/subsystems/BatteryBank.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.lang.reflect.Array;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BatteryBank extends SubsystemBase {
+
+  Account[] accounts = {
+    new Account("Shooter", 0, 0, 0, 0), new Account("Hopper", 0 ,0 ,0 ,0), 
+    new Account("Intake", 0, 0, 0, 0), new Account("Chassis", 0, 0, 0, 0),
+    new Account("Climber", 0, 0, 0, 0)
+  };
+
+  public Account openAccount(String name, double priority){
+    Account newAccount = new Account(name, priority, 0, 0, 0);
+    return newAccount;
+  }
+  /** Creates a new BatterBank. */
+  public BatteryBank() {} 
+  @Override
+  public void periodic() {
+  
+    // This method will be called once per scheduler run
+  }
+}
