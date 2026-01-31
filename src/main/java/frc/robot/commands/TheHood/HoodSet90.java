7928a381ccdb493e1401a626276472f213965818
Adamxyleng <adam.xy.leng@gmail.com>
2026-01-30 19:36:34 -0600

    added hood commands

--- src/main/java/frc/robot/commands/TheHood/HoodSet90.java
+++ src/main/java/frc/robot/commands/TheHood/HoodSet90.java
@@ -2,14 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Shooter;
+package frc.robot.commands.TheHood;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class ShooterBasicForward extends Command {
-  /** Creates a new ShooterBasicForward. */
-  public ShooterBasicForward() {
+public class HoodSet90 extends Command {
+  /** Creates a new HoodSet90. */
+  public HoodSet90() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
