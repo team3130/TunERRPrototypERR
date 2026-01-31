df9085df7fb0be9fbb6800ebd412a441834a3e4a
MrBeastHeHeHeHa <titusmoua01@gmail.com>
2026-01-31 15:07:25 -0600

    runintake + robotcontainer changes
    forgot to save oops

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.lang.ModuleLayer.Controller;
+
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
@@ -22,18 +24,23 @@ import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Constants.Swerve;
 import frc.robot.commands.DriveWithTransPID;
+import frc.robot.commands.ReverseIntake;
+import frc.robot.commands.RunIntake;
 import frc.robot.commands.RunTalonFX;
 import frc.robot.commands.RunTalonSRX;
 import frc.robot.commands.RunVictor;
 import frc.robot.commands.TeleopDrive;
 import frc.robot.commands.ToggleHubTargeting;
 import frc.robot.commands.UpdateOdoFromVision;
+import frc.robot.commands.RunIntake;
+import frc.robot.commands.ReverseIntake;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.MultiUseTalonFX;
 import frc.robot.subsystems.MultiUseTalonSRX;
 import frc.robot.subsystems.MultiUseVictor;
+import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -42,7 +49,7 @@ public class RobotContainer {
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDriveRequestType(DriveRequestType.Velocity); // Use open-loop control for drive motors
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
@@ -61,6 +68,10 @@ public class RobotContainer {
     public final MultiUseTalonFX falcon1;
     public final MultiUseTalonFX falcon2;
 
+    private final Intake intake;
+    private final RunIntake runIntake;
+    private final ReverseIntake reverseIntake;
+
     private final SendableChooser<Command> autoChooser;
 
     private final DriveWithTransPID command = new DriveWithTransPID(drivetrain, drive);
@@ -74,6 +85,10 @@ public class RobotContainer {
 
         falcon1 = new MultiUseTalonFX(30);
         falcon2 = new MultiUseTalonFX(31);
+
+        intake = new Intake();
+        runIntake = new RunIntake();
+        reverseIntake = new ReverseIntake();
 
         configureBindings();
 
@@ -106,6 +121,9 @@ public class RobotContainer {
         commandDriverController.L1().whileTrue(new RunTalonFX(falcon2, 1));
 
         commandDriverController.R1().onTrue(new ToggleHubTargeting(drivetrain));
+
+        commandDriverController.L2().whileTrue(new RunIntake(runIntake));
+        commandDriverController.R2().whileTrue(new ReverseIntake(reverseIntake));
 
         // reset the field-centric heading
         commandDriverController.povUp().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
--- src/main/java/frc/robot/commands/RunIntake.java
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -5,12 +5,19 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class RunIntake extends Command {
+  private final Intake intake;
+  private final CommandPS5Controller controller;
   /** Creates a new RunIntake. */
-  public RunIntake() {
+  public RunIntake(Intake intake, CommandPS5Controller controller) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.intake = intake;
+    this.controller = controller;
+    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
@@ -19,15 +26,17 @@ public class RunIntake extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
-
+  public void execute() {
+    intake.runIntake();
+  }
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
-
+  public void end(boolean interrupted) {
+    intake.stopIntake();
+  }
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return !controller.R2().getAsBoolean(); // placeholder
   }
 }
