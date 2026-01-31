edf41e380223443433cc8c82f4165f8e18ed4c5d
MrBeastHeHeHeHa <titusmoua01@gmail.com>
2026-01-31 15:06:12 -0600

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,13 +5,45 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
-  public Intake() {}
+    private final TalonFX intake;
+    private double intakeSpeed = 0.5;
+    public intake() {
+        intake = new TalonFX(Constants.CAN.Falcon);
+        intake.configFactoryDefault();
+        intake.setInverted(true);
+    } 
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public void runIntake() {
+        intake.set(intakeSpeed);
+    }
+
+    public void reverseIntake() {
+        intake.set(-intakeSpeed);
+    }
+
+    public void stopIntake() {
+        intake.set(0);
+    }
+
+    public double getIntakeSpeed() {
+        return intakeSpeed;
+    }
+
+    public void setIntakeSpeed(double speed) {
+        intakeSpeed = speed;
+    }
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
 }
