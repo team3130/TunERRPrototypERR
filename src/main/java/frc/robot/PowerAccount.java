617fc7dbffc646546999e6d767c1e09620001a83
CorianderSeed <github.unscrew076@slmails.com>
2026-01-31 22:02:51 -0600

    Added max and min req power to account class. Added logic to account for power requested < maxpower.

--- src/main/java/frc/robot/PowerAccount.java
+++ src/main/java/frc/robot/PowerAccount.java
@@ -3,22 +3,26 @@ package frc.robot;
 public class PowerAccount {
 
     private String name;
-    private double request;
+    private double minRequest;
+    private double maxRequest;
     private double allowance;
     private int priority;
 
-    public PowerAccount(String name, double request, double allowance, int priority) {
+    public PowerAccount(String name, double minRequest, double maxRequest, double allowance, int priority) {
         this.name = name;
-        this.request = request;
+        this.minRequest = minRequest;
+        this.maxRequest = maxRequest;
         this.allowance = allowance;
         this.priority = priority;
     }
 
     public String getName() { return name; }
-    public double getRequest() { return request; }
+    public double getMinRequest() { return minRequest; }
+    public double getMaxRequest() { return maxRequest;}
     public double getAllowance() { return allowance; }
     public int getPriority() { return priority; }
 
     public void setAllowance(double allowance) { this.allowance = allowance; }
-    public void setRequest(double request) { this.request = request; }
+    public void setMinRequest(double request) { this.minRequest = request; }
+    public void setMaxequest(double request) { this.maxRequest = request; }
 }
