41aadbb1ce5663c157a70f562dc5e87f2fcb76d6
Rohan-Kuruvila <151589950+Rohan-Kuruvila@users.noreply.github.com>
2026-02-04 19:14:27 -0600

    TestPowerBank and Test Folder Created

---
+++ src/test/java/frc/robot/TestPowerBank.java
@@ -0,0 +1,71 @@
+package frc.robot;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPowerBank {
+    @Test
+    public void firstTest() {
+        PowerBank powerBank = new PowerBank(3);
+
+        PowerAccount hungry = powerBank.openAccount("Hungry", 1);
+        PowerAccount walking = powerBank.openAccount("Walking", 50);
+        PowerAccount important = powerBank.openAccount("Important", 1000);
+
+        hungry.setMinRequest(20);
+        hungry.setMaxequest(300);
+        walking.setMinRequest(20);
+        walking.setMaxequest(100);
+        important.setMinRequest(30);
+        important.setMaxequest(30);
+
+        powerBank.calculateAllowance(powerBank.distributeMinRequest());
+
+        System.out.println("Hungry Allowance: " + hungry.getAllowance());
+        System.out.println("Walking Allowance: " + walking.getAllowance());
+        System.out.println("Important Allowance: " + important.getAllowance());
+    }
+
+    @Test
+    public void overPowerTest() {
+        PowerBank powerBank = new PowerBank(3);
+
+        PowerAccount hungry = powerBank.openAccount("Hungry", 1);
+        PowerAccount walking = powerBank.openAccount("Walking", 50);
+        PowerAccount important = powerBank.openAccount("Important", 1000);
+
+        hungry.setMinRequest(20);
+        hungry.setMaxequest(700);
+        walking.setMinRequest(20);
+        walking.setMaxequest(100);
+        important.setMinRequest(30);
+        important.setMaxequest(30);
+
+        powerBank.calculateAllowance(powerBank.distributeMinRequest());
+
+        System.out.println("Hungry Allowance: " + hungry.getAllowance());
+        System.out.println("Walking Allowance: " + walking.getAllowance());
+        System.out.println("Important Allowance: " + important.getAllowance());
+    }
+
+    @Test
+    public void overPowerTest2() {
+        PowerBank powerBank = new PowerBank(3);
+
+        PowerAccount hungry = powerBank.openAccount("Hungry", 1);
+        PowerAccount walking = powerBank.openAccount("Walking", 50);
+        PowerAccount important = powerBank.openAccount("Important", 1000);
+
+        hungry.setMinRequest(20);
+        hungry.setMaxequest(700);
+        walking.setMinRequest(20);
+        walking.setMaxequest(100);
+        important.setMinRequest(30);
+        important.setMaxequest(30.1);
+
+        powerBank.calculateAllowance(powerBank.distributeMinRequest());
+
+        System.out.println("Hungry Allowance: " + hungry.getAllowance());
+        System.out.println("Walking Allowance: " + walking.getAllowance());
+        System.out.println("Important Allowance: " + important.getAllowance());
+    }
+}
