ac4207ac43e74b02a0c4e5c87451619a897d2bab
skwwking3 <sihyuk.kim2011@gmail.com>
2026-02-07 13:53:19 -0600

    Update VerticalHopper.java

--- src/main/java/frc/robot/subsystems/VerticalHopper.java
+++ src/main/java/frc/robot/subsystems/VerticalHopper.java
@@ -25,45 +25,54 @@ public class VerticalHopper extends SubsystemBase {
     private final TalonFX hoppervertical;
     private final MotionMagicVelocityVoltage voltRequest;
     private double verticalSpeed = 0.3;
-    private double accelerationMetersPerSecSquared = 49;
-    private final double accelerationRotations = Units.radiansToRotations(accelerationMetersPerSecSquared/Units.inchesToMeters(2));
+    private double accelerationMetersPerSecSquared = 49; // m/s^2 (target linear acceleration)
+    private final double accelerationRotations =
+          Units.radiansToRotations(accelerationMetersPerSecSquared / Units.inchesToMeters(1)); // convert to rot/s^2 (angular accerelation)
 
-    private double targetVelocityMetersPerSec = 11.25;
-    private final double targetVelocityRotations = Units.radiansToRotations(targetVelocityMetersPerSec/Units.inchesToMeters(2));
+    private double targetVelocityMetersPerSec = 11.25; // target linear velocity (m/s)
+    private final double targetVelocityRotations = Units.radiansToRotations(targetVelocityMetersPerSec / Units.inchesToMeters(1)); // convert to rot/s
 
     public VerticalHopper() {
-        hoppervertical = new TalonFX(34); // not necessary but just in case
+        hoppervertical = new TalonFX(34);
         hoppervertical.getConfigurator().apply(new TalonFXConfiguration().withMotorOutput(new MotorOutputConfigs()
         .withNeutralMode(NeutralModeValue.Coast).withInverted(InvertedValue.Clockwise_Positive)));
-        voltRequest = new MotionMagicVelocityVoltage(0);
+        voltRequest = new MotionMagicVelocityVoltage(0); 
+        TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+        Slot0Configs slot0Configs = talonFXConfigs.Slot0;
+        slot0Configs.kS = 0;
+        slot0Configs.kV = 0.12;
+        slot0Configs.kA = 0.01;
+        slot0Configs.kP = 0;
+        slot0Configs.kI = 0;
+        slot0Configs.kD = 0;
     }
-    
+
     public void revAtVelocity(double velocityMetersPerSec) {
-        double radsPerSec = velocityMetersPerSec / Units.inchesToMeters(2);
-        double rotsPerSec = Units.radiansToRotations(radsPerSec);
-        hoppervertical.setControl(voltRequest.withVelocity(rotsPerSec));  
+        double radsPerSec = velocityMetersPerSec / Units.inchesToMeters(1); // v = w * r -> w = v / r
+        double rotsPerSec = Units.radiansToRotations(radsPerSec); // rad/s -> rot/s
+        hoppervertical.setControl(voltRequest.withVelocity(rotsPerSec)); // Motion Magic velocity target
     }
     public void rev() {
-        hoppervertical.setControl(voltRequest.withVelocity(targetVelocityRotations));
+        hoppervertical.setControl(voltRequest.withVelocity(targetVelocityRotations)); // stored target velocity
     }
 
     public double getVelocity() {
-        double rotsPerSec = hoppervertical.getVelocity().getValueAsDouble();
-        double radsPerSec = Units.rotationsToRadians(rotsPerSec);
-        double metersPerSec = radsPerSec * Units.inchesToMeters(2);
+        double rotsPerSec = hoppervertical.getVelocity().getValueAsDouble(); // motor rot/s
+        double radsPerSec = Units.rotationsToRadians(rotsPerSec); // rot/s -> rad/s
+        double metersPerSec = radsPerSec * Units.inchesToMeters(1); // v = w * r
         return metersPerSec;
     }
     public double getAcceleration() {
-        double rotsPerSecSquared = hoppervertical.getAcceleration().getValueAsDouble();
-        double radsPerSecSquared = Units.rotationsToRadians(rotsPerSecSquared);
-        double metersPerSecSquared = radsPerSecSquared * Units.inchesToMeters(2);
+        double rotsPerSecSquared = hoppervertical.getAcceleration().getValueAsDouble(); // motor rot/s^2
+        double radsPerSecSquared = Units.rotationsToRadians(rotsPerSecSquared); // rot/s^2 -> rad/s^2
+        double metersPerSecSquared = radsPerSecSquared * Units.inchesToMeters(1); // a = alpha * r
         return metersPerSecSquared;
     }  
-    public double getTargetAcceleration() {return accelerationMetersPerSecSquared;}
-    public void setTargetAcceleration(double value) {accelerationMetersPerSecSquared = value;}
+    public double getTargetAcceleration() {return accelerationMetersPerSecSquared;} // m/s^2 target accel
+    public void setTargetAcceleration(double value) {accelerationMetersPerSecSquared = value;} // update target accel
 
-    public double getTargetVelocity() {return targetVelocityMetersPerSec;}
-    public void setTargetVelocity(double value) {targetVelocityMetersPerSec = value;}
+    public double getTargetVelocity() {return targetVelocityMetersPerSec;} // m/s target velocity
+    public void setTargetVelocity(double value) {targetVelocityMetersPerSec = value;} // update target velocity
     
     public void runHopperVertical() {
         hoppervertical.set(verticalSpeed);
